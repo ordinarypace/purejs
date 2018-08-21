167be19d121262d9d8a1af3bf617831358603d70
JeongSik Jang <js.ordinary@daum.net>
2018-08-21 13:42:00 +0900

    update purejs library

--- helpers/index.js
+++ helpers/index.js
@@ -1,346 +1,282 @@
-import Md from 'mobile-detect';
+const error = str => {
+    throw new Error(str ? str : 'Must be override');
+};
 
-const Helpers = (_ => {
-    const elementsCache = new Map();
-    const helpers = {
-        string: {
-            /**
-             * @desc 문자열의 길이를 체크하여 Boolean 을 반환한다.
-             * @param str
-             */
-            isEmpty(str){
-                if(!this.isPrimitive(str)) helpers.err('Type must be STRING!');
-                return str.trim().length;
-            },
 
-            /**
-             * @desc 문자 타입을 검사하여 Boolean 을 반환한다.
-             * @param str
-             * @returns {boolean}
-             */
-            isPrimitive(str){
-                return typeof str === 'string';
-            }
-        },
+const Singleton = class extends WeakMap {
+    has(){
+        error();
+    }
+    get(){
+        error();
+    }
+    set(){
+        error();
+    }
+    getInstance(v){
+        if(!super.has(v.constructor)) super.set(v.constructor, v);
+        return super.get(v.constructor);
+    }
+};
 
-        object: {
-            /**
-             * @desc 객체에 해당 프로퍼티 존재 유무 검사하여 Boolean 을 반환한다.
-             * @param p
-             * @param c
-             * @returns {boolean}
-             */
-            has(p, c){
-                return p.hasOwnProperty(c);
-            },
+const singleton = new Singleton();
 
-            /**
-             * @desc 객체를 순회하며 해당 프로퍼티 검사 후 리턴 타입에 맞게 상위 객체의 key or 해당 프로퍼티를 반환한다.
-             * @param o (검사할 오브젝트)
-             * @param t (타겟이 될 프로퍼티)
-             * @param p (parent key 리턴 유무)
-             * @returns {*}
-             */
-            find(o, t, p){
-                let node = null;
+const Js = class {
+    constructor(){
+        this._dom = null;
+        this._events = {
+            events: '',
+            callback: null,
+            capture: false
+        };
 
-                for(const k of Object.keys(o)){
-                    if(this.has(o, k)){
-                        if(typeof o[k] === 'object' && (node = o[k])) while(node){
-                            const keys = Object.keys(node);
+        return singleton.getInstance(this);
+    }
 
-                            if(keys.includes(t)) return p ? k : t;
-                        }
-                    }
-                }
-            },
+    /**
+     * @desc 노드 환경일 경우 환경 현재 환경 반환
+     * @returns {*}
+     */
+    get env(){
+        return process.env.NODE_ENV
+    }
 
-            /**
-             * @desc 객체 인스턴스에 타겟이 될 객체의 getter 를 생성한다.
-             * @param c
-             * @param o
-             * @returns {*}
-             */
-            extend(c = {}, o){
-                return this.prop(c, Object.keys(o).reduce((p, c) => { return this.prop(p, { get [c](){ return p[c] } }), p; }, o));
-            },
+    /**
+     * @desc 셀렉터
+     * @param selector
+     * @param parent
+     * @returns {Js}
+     */
+    $(selector, parent){
+        if(!selector) error('Must be selector');
 
-            /**
-             * @desc 타겟에 프로퍼티를 설정한다.
-             * @param t
-             * @param p
-             */
-            prop(t, p){
-                return Object.assign(t, p);
-            }
-        },
+        this._dom = Array.from((parent || document).querySelectorAll(selector));
 
-        func: {
-            /**
-             * @desc function 의 이름을 얻는다.
-             * @param v
-             * @returns {string | *}
-             */
-            name(v){
-                return (v.toString().match(/^function\s*([^\s(]+)/) || [])[1];
-            }
-        },
+        return this;
+    }
 
-        query: {
-            /**
-             * @desc query string 생성기.
-             * @param o
-             * @returns {*|string}
-             */
-            join(o){
-                return Object.keys(o).reduce((p, c) => { p.push(`${c}=${o[c]}`), p; }, []).join('&');
-            },
+    /**
+     * @desc 이벤트 on, off 헬퍼
+     * @param command
+     */
+    bind(command){
+        if(this._dom.length > 1) this._dom.forEach(v => this._events.events.split(',').forEach(e => v[command](e.trim(), this._events.callback, this._events.capture)));
 
-            /**
-             * @desc query string 얻기.
-             */
-            params(){
-                const params = {};
+        this._events.events.split(',').forEach(v => this._dom[command](v, this._events.callback, this._events.capture));
+    }
 
-                location.search.substr(1).split('&').forEach(v => {
-                    const [key, value] = v.split('=');
+    /**
+     * @desc 이벤트 on
+     * @param events
+     * @param callback
+     * @param capture
+     */
+    on(events, callback, capture){
+        this.props(this._events, { events, callback, capture });
 
-                    params[key] = value;
-                });
+        if(!this._dom.length) error('Must be 1 element');
 
-                return params;
-            }
-        },
+        this.bind('addEventListener');
+    }
 
-        number: {
-            /**
-             * @desc locale 에 맞는 숫자 콤마 포맷을 반환한다.
-             * @param n
-             * @returns {string}
-             */
-            format(n){
-                return n.toLocaleString();
-            }
-        },
+    /**
+     * @desc 이벤트 off
+     */
+    off(){
+        this.bind('removeEventListener');
+    }
 
-        storage: {
-            set type(v){
-                this._storage = v === 'session' ? sessionStorage : localStorage;
-            },
+    /**
+     * @desc element 가 존재할 경우 하위 자식 찾기
+     * @param selector
+     */
+    find(selector){
+        if(this._dom.length > 1) error('mandatory in 1 parameter');
 
-            get type(){
-                return this._storage;
-            },
+        this._dom.querySelectorAll(selector);
+    }
 
-            set(k, v){
-                if(!helpers.string.isPrimitive(k)) helpers.err('Key must be string type!!!');
+    /**
+     * @desc default prevent
+     * @param e
+     * @returns {Js}
+     */
+    prevent(e){
+        e.preventDefault();
+        return this;
+    }
 
-                this._storage.setItem(k, JSON.stringify(v));
-            },
+    /**
+     * @desc stop propagation event
+     * @param e
+     */
+    stop(e){
+        e.stopPropagation();
+        return this;
+    }
 
-            get(k){
-                if(!helpers.string.isPrimitive(k)) helpers.err('Key must be string type!!!');
+    /**
+     * @desc 객체의 프로퍼티 확장
+     * @param target
+     * @param properties
+     * @returns {*}
+     */
+    props(target, properties){
+        return Object.assign(target, ...properties);
+    }
 
-                return JSON.parse(this._storage.getItem(k));
-            },
+    /**
+     * @desc dataset 가져오기, 설정
+     * @param data
+     * @param value
+     * @returns {*}
+     */
+    dataset(data, value){
+        const string =  data.replace(/\-([a-z])/ig, (m, letter) => letter.toUpperCase());
 
-            remove(k){
-                if(!helpers.string.isPrimitive(k)) helpers.err('Key must be string type!!!');
+        if(this._dom.hasOwnProperty('dataset')) return value ? this._dom.dataset[string] = value : this._dom.dataset[string];
+        else return value ? this._dom.setAttribute(`data-${data}`, value) : this._dom.getAttribute(`data-${data}`);
 
-                this._storage.removeItem(k);
-            },
+        return this;
+    }
 
-            clear(){
-                this._storage.clear();
-            },
+    /**
+     * @desc 배열 판별
+     * @param arr
+     * @returns {boolean}
+     */
+    isArray(arr){
+        return Array.isArray(arr);
+    }
 
-            has(k){
-                return helpers.object.has(this._storage, k);
-            }
-        },
+    /**
+     * @desc 빈 값 판별
+     * @param obj
+     * @returns {Number}
+     */
+    empty(obj){
+        if(typeof obj === 'string') return obj.trim().length;
 
-        $: {
-            /**
-             * @desc 부모 노드가 있으면 부모 노드에서 자식 노드를 찾아서 반환, 없으면 document 에서 찾아서 반환한다. 길이가 1일 경우 node 자체를 반환 하며 이상일 경우 Array 타입으로 변경하여 반환한다. (caching 추가)
-             * @param target
-             * @param parent
-             * @returns {Array}
-             */
-            get(target, parent, isCache){
-                if(isCache && elementsCache.has(target)) return elementsCache.get(target);
-                else {
-                    const el = (parent || document).querySelectorAll(target);
-                    const result = el.length === 1 ? el[0] : Array.from(el);
+        if(this.isArray(obj)) return obj.length;
 
-                    elementsCache.set(target, result);
+        return Object.keys(obj).length;
+    }
 
-                    return result;
-                }
-            },
+    /**
+     * @desc append children
+     * @param parent
+     * @param elements
+     */
+    append(parent, ...elements){
+        return elements.forEach(v => (typeof parent === 'string' && this.$(parent) || parent).appendChild(v)), parent;
+    }
 
-            /**
-             * @desc 엘리먼트를 생성하고 타입에 맞는 프로퍼티를 설정한다.
-             * @param tag
-             * @param attr
-             * @returns {HTMLElement}
-             */
-            el(tag, ...attr){
-                const el = typeof tag === 'string' ? document.createElement(tag) : tag;
+    /**
+     * @desc prepend children
+     * @param parent
+     * @param elements
+     */
+    prepend(parent, ...elements){
+        return elements.reverse().forEach(v => (typeof parent === 'string' && this.$(parent) || parent).insertBefore(v, parent.firstChild)), parent;
+    }
 
-                for(let i = 0; i < attr.length;){
-                    const k = attr[i++], v = attr[i++];
+    /**
+     * @desc 세자리 수 콤마 삽입
+     * @param number
+     * @returns {*}
+     */
+    numberFormat(number){
+        if(!number) return number;
 
-                    switch(true){
-                        case typeof el[k] === 'function': {
-                            el[k](...(Array.isArray(v) ? v : [v]));
-                            break;
-                        }
-                        case k[0] === '@': {
-                            el.style[k.substr(1)] = v;
-                            break;
-                        }
-                        default: {
-                            el[k] = v;
-                            break;
-                        }
-                    }
-                }
-                return el;
-            },
+        return number.toString().replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,');
+    }
 
-            /**
-             * @desc 트리를 타입에 따라 순회하며 원하는 노드를 찾아서 리턴한다.
-             * @param item
-             * @param className
-             * @param type: 'next', 'prev', default: child
-             * @returns {*}
-             */
-            find({ target, className, type, tag }){
-                let node = target;
+    /**
+     * @desc 객체를 쿼리 스트링으로 변환
+     * @param o
+     * @returns {*|string}
+     */
+    query(o){
+        return Object.keys(o).reduce((p, c) => { return p.push(`${c}=${o[c]}`), p; }, []).join('&');
+    }
 
-                while(node){
-                    if(node.nodeType === 1 && (!className ? node.tagName.toLowerCase() === tag : node.classList.contains(className))) return node;
+    /**
+     * @desc 쿼리 스트링 얻기
+     */
+    params(url){
+        const params = {}, scheme = (url ? url : location.search).split('?');
 
-                    node = {
-                        next: node.nextSibling,
-                        prev: node.previousSibling,
-                        parent: node.parentNode
+        if(scheme.length > 1){
+            (url ? url : location.search).split('?')[1].split('&').forEach((v, i) => {
+                const [key, value] = v.split('=');
 
-                    }[type] || node.children;
-                }
-            },
+                params[key] = value;
+                params.length = i + 1;
+            });
 
-            /**
-             * @desc 커스텀 데이터 헬퍼.
-             * @param target
-             * @param data
-             * @param value
-             * @returns {*}
-             */
-            dataset(target, data, value){
-                const string =  data.replace(/\-([a-z])/ig, (m, letter) => letter.toUpperCase());
+        } else params.length = 0;
 
-                if(target.hasOwnProperty('dataset')) return value ? target.dataset[string] = value : target.dataset[string];
-                else return value ? target.setAttribute(`data-${data}`, value) : target.getAttribute(`data-${data}`);
-            },
+        return params;
+    }
 
-            /**
-             * @desc append node
-             * @param p
-             * @param el
-             */
-            append(p, ...el){
-                return el.forEach(v => (typeof p === 'string' && helpers.$.get(p) || p).appendChild(v)), p;
-            },
+    /**
+     * @desc function 이름 얻기
+     * @param v
+     * @returns {*}
+     */
+    name(v){
+        return (v.toString().match(/^function\s*([^\s(]+)/) || [])[1];
+    }
 
-            /**
-             * @desc prepend node
-             * @param p
-             * @param el
-             */
-            prepend(p, ...el){
-                return el.reverse().forEach(v => (typeof p === 'string' && helpers.$.get(p) || p).insertBefore(v, p.firstChild)), p;
-            }
-        },
+    /**
+     * @desc 쿠키 얻기
+     * @param cname
+     * @returns {*}
+     */
+    getCookie(cname){
+        const name = cname + "=";
+        const decodedCookie = decodeURIComponent(document.cookie);
+        const ca = decodedCookie.split(';');
 
-        event: {
-            init(event, target, callback, capture){
-                helpers.object.prop(this, { event, target, callback, capture });
-            },
+        for(let i = 0; i < ca.length; i += 1) {
+            let c = ca[i];
 
-            /**
-             * @desc 이벤트 리스너.
-             * @param type
-             * @param target
-             * @param callback
-             * @param capture
-             */
-            on(event, target, callback, capture){
-                if(!this.event) this.init(event, target, callback, capture);
+            while(c.charAt(0) == ' ') c = c.substring(1);
 
-                if(Array.isArray(this.target)) this.target.forEach(v => this.event.forEach(e => v.addEventListener(e, this.callback, this.capture)));
-                else this.event.forEach(e => Helpers.$.get(this.target).addEventListener(e, this.callback, this.capture));
-
-                return this;
-            },
-
-            /**
-             * @desc 이벤트 prevent.
-             * @param e
-             * @returns {helpers}
-             */
-            prevent(e){
-                e.preventDefault();
-                return this;
-            },
-
-            /**
-             * @desc 이벤트 stop propagation.
-             * @param e
-             * @returns {helpers}
-             */
-            stop(e){
-                e.stopPropagation();
-                return this;
-            }
-        },
-
-        /**
-         * @desc throw error.
-         * @param v
-         */
-        err(v = 'invalid'){
-            throw v;
-        },
-
-        /**
-         * @desc instanceof 판별.
-         * @param t
-         * @param p
-         * @returns {boolean}
-         */
-        is(t, p){
-            return t instanceof p;
-        },
-
-        isLogged(){
-            helpers.storage.type = 'session';
-            return helpers.storage.has('session') ? helpers.storage.get('session') : false;
-        },
-
-        /**
-         * @desc PC / Mobile 판별.
-         */
-        isMobile(){
-            let md;
-
-            if(!md) md = new Md(window.navigator.userAgent);
-
-            return md.mobile();
+            if(c.indexOf(name) == 0) return c.substring(name.length, c.length);
         }
-    };
+        return "";
+    }
 
-    return helpers;
-})();
+    /**
+     * @desc 쿠키 설정
+     * @param cname
+     * @param cvalue
+     * @param exdays
+     */
+    setCookie(cname, cvalue, exdays){
+        const d = new Date();
 
-export default Helpers;
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+
+        const expires = "expires="+d.toUTCString();
+
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+
+    /**
+     * @desc 인스턴스 포함 여부 확
+     * @param target
+     * @param parent
+     * @returns {boolean}
+     */
+    is(target, parent){
+        return target instanceof parent;
+    }
+};
+
+const js = new Js();
+
+window.addEventListener('load', _ => {
+    window.js = js;
+});
